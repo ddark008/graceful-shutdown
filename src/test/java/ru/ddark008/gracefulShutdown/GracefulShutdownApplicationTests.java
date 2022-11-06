71e3c93407176057eee9eeadd7412a914a3d77b7
Дмитрий А. Елисеев <i@ddark008.ru>
2022-11-06 09:38:54 +0300

    feat: spring template

---
+++ src/test/java/ru/ddark008/gracefulShutdown/GracefulShutdownApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddark008.gracefulShutdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GracefulShutdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
